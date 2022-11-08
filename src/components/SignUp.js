b96ad2d651253f5f1d7d727ed0da60cbb7ee43f1
Winston Langat <winston.langat@student.moringaschool.com>
2022-11-08 14:38:05 +0300

    Progress

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Signup.css";
 import { useRef, useState,useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 import { fontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const USER_REGEX = /^[A-z][A-z0-9-_]{4,10}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = '/register';
 
-function SignUp() {
+const SignUp = () => {
+  const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+    
+    useEffect(() => {
+      userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+      setValidName(USER_REGEX.test(user));
+  }, [user])
+
+  useEffect(() => {
+      setValidPwd(PWD_REGEX.test(pwd));
+      setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd])
+
+  useEffect(() => {
+      setErrMsg('');
+  }, [user, pwd, matchPwd])
+
   return (
     <div>SignUp</div>
   )
