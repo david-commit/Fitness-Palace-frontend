1399043ab13b4dd10ddca65ae115a41dd2457a82
saidsoud72 <said.omar@student.moringaschool.com>
2022-11-08 11:41:30 +0300

    create Homepage file

---
+++ src/components/HomepagePricing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomepagePricing.css"
+function HomepagePricing() {
+    return (
+      <div>HomepagePricing blah blah blah</div>
+    )
+  }
+  
+  export default HomepagePricing
+
+  
