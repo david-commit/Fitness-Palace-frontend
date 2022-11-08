777ba940584d21512af72d80a33485d2e79ab302
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-11-08 11:57:28 +1100

    create a form

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -4,19 +4,24 @@ import "./Appointment.css"
 
 function Appointment() {
   return (
-      <div>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input type="text" name="username" placeholder="Username" />
-          </div>
-          <div>
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-
+    <div>
+      <h1>Appointment</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+          <input type="date" name="date" placeholder="Date" />
+        </div>
+        <div>
+          <input type="date" name="date" placeholder="Date" />
+        </div>
+        <div>
+          <input type="date" name="date" placeholder="Date" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
 }
 
