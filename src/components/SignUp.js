998c483a493fa9f6aee8c39108b4ee47973ae956
Winston Langat <winston.langat@student.moringaschool.com>
2022-11-08 13:19:03 +0300

    Dependencies

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react";
 import "./Signup.css";
+import { useRef, useState,useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import { fontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
 
 function SignUp() {
   return (
