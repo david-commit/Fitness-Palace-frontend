2e2c3cf7a613cefd33b74385c73de2de9286f18d
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-11-09 10:56:41 +1100

    edit the styling

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -6,24 +6,28 @@ function Appointment() {
   return (
     <div>
       <h1>Appointment</h1>
-        <div>
-          <input type="text" name="name" placeholder="Name" />
-        </div>
-        <div>
-          <input type="date" name="date" value="" placeholder="Date" />
-        </div>
-        <div>
-          <input
-            type="datetime-local"
-            name="duration"
-            value=""
-            placeholder="Duration"
-          />
-        </div>
-        <div>
-          <input type="text" name="trainor" placeholder="Trainor" />
-        </div>
-        <input type="submit" value="Book" />
+      <div>
+        <input type="text" name="name" placeholder="Name" />
+      </div>
+      <div className="time">
+        <input type="date" name="date" value="" placeholder="Date" />
+      
+      
+        <input
+          type="datetime-local"
+          name="duration"
+          value=""
+          placeholder="Duration"
+        />
+      </div>
+      <div>
+        <input type="text" name="trainor" placeholder="Trainor" />
+      </div>
+      <div>
+        <button className="button" type="submit">
+          BOOK
+          </button>
+      </div>
     </div>
   );
 }
