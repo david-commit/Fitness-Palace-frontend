dbe84be57fb08f6624da4a6c2475080c9987231a
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-11-08 12:15:40 +0300

    added an invite icon

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,15 @@ import "./Welcome.css"
 
 function Welcome() {
   return (
-    <div>.....</div>
+    <div className='Invites'>
+     <p>
+     <i class="fa-solid fa-check"></i>
+    </p>
+
+    <h2>YOUR SESSION HAS BEEN BOOKED</h2>
+    <h3>BOOKING DETAILS</h3>
+
+    </div>
   )
 }
 
