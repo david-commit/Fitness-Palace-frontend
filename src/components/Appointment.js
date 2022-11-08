09fb2b50a353e0bec6678d2610a38baee4e28b8a
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-11-08 11:54:51 +1100

    modify appointment file

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -4,10 +4,20 @@ import "./Appointment.css"
 
 function Appointment() {
   return (
-    <div>
-      Appointment
-    </div>
-  )
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+
+  );
 }
 
 export default Appointment
