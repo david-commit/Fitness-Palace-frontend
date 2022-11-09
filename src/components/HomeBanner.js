b776f32a52791d9ae6f9116c7f247a3556a9a882
Benson <benson.odongo@student.moringaschool.com>
2022-11-09 10:13:15 +0300

    Home banner

---
+++ src/components/HomeBanner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./HomeBanner.css";
+
+function HomeBanner() {
+  return (
+    <div>
+      <div class="banner">
+        <img src="./img/jump-fitness-workout.png" alt="Homebanner" />
+      </div>
+      <div class="banner-text">
+        <h1>THE BEST FITNESS STUDIO IN TOWN</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+          excepturi ratione harum placeat sint, adipisci eaque tenetur fuga
+          asperiores ea esse dicta, non sed ex quae quisquam laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HomeBanner;
