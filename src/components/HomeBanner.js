417763c51c7b2fb62d95598a7aa3b85d61b0e9e3
Benson <benson.odongo@student.moringaschool.com>
2022-11-09 10:25:38 +0300

    Home banner modified

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./HomeBanner.css";
+import bannerimg from "../img/jump-fitness-workout1.png"
 
 function HomeBanner() {
   return (
-    <div>
-      <div class="banner">
-        <img src="./img/jump-fitness-workout.png" alt="Homebanner" />
+    <div className="home-banner">
+      <div className="home-banner-img">
+        <img src={bannerimg} alt="Banner" />
       </div>
-      <div class="banner-text">
+      <div className="home-banner-content">
         <h1>THE BEST FITNESS STUDIO IN TOWN</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
           excepturi ratione harum placeat sint, adipisci eaque tenetur fuga
           asperiores ea esse dicta, non sed ex quae quisquam laborum.
         </p>
+        <button type="button">BOOK APPOINTMENT</button>
       </div>
     </div>
   );
